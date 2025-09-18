7eeb90a5723f0ac73b439f68804d5c9f9347e090
Arthur Amorim <arthur.silva3@iesb.edu.br>
2025-09-18 10:49:57 -0300

    Adicionand aula07

--- aula/aula07/app.js
+++ aula/aula07/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
@@ -8,10 +7,18 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.urlencoded({ extended: false}));
 
 app.use('/', indexRouter);
 
+const tarefas = [];
+
+app.get('/tarefas', (req, res) => {
+    res.json(tarefas);
+})
+
+app.post('/tarefas', (req, res) => {
+    res.status(201).json({id:1,nome:"Estudar para P1",concluida:false});
+})
 
 module.exports = app;
